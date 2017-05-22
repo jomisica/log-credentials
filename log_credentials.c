1b1b723c0c61d8bec850f7081fbceeaf9119227f
José Miguel Silva Caldeira <miguel@ncdc.pt>
2017-05-22 23:53:26 +0100

    Move from COPYNG to LICENSE.

--- log_credentials.c
+++ log_credentials.c
@@ -12,7 +12,7 @@
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with this program - see the file COPYING.
+ *  along with this program - see the file LICENSE.
  *
  * As figuras imaginárias têm mais relevo e verdade que as reais.
  * "Fernando Pessoa"
