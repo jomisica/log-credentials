434a871558436a376ef3d544b6e685ccc111712a
José Miguel Silva Caldeira <miguel@ncdc.pt>
2017-05-22 23:42:50 +0100

    Organize and make the project available in github.

---
+++ README.md
@@ -0,0 +1,45 @@
+# log-credentials
+
+	PAM log-credentials has been written by José Miguel Silva Caldeira <miguel@ncdc.pt>.
+
+## Description:
+	PAM log-credentials allows you to log credentials after authorization.
+	It will log messages like that:
+	May 22 18:50:15 localhost log-credentials: host=xxx.xxx.xxx.xxx service=sshd user=root pass=xxxxxxx.
+
+	The idea of this module is to facilitate obtaining information that allows you
+	to analyze brute force attacks on ssh and other services.
+
+	Through these data, with scripts and in a simple way it is possible to perceive:
+	From where the attacks are initiated, the countries, the networks, etc. Through IP;
+	You can see which users are most used in login attempts;
+	The type of passwords that are used in attack attempts;
+	Among others.
+
+	In this way it is possible for the administrator to be aware and to take measures
+	so that good practices are taken to avoid these attacks.
+
+	Use at your own risk!
+
+## Installation
+
+### Using Git
+
+	You can clone the repository wherever you want. (I like to keep it in
+	`~/Projects/log-credentials`.)
+
+```bash
+	$ git clone https://github.com/jomisica/log-credentials.git
+	$ cd log-credentials
+	$ libtoolize --force
+	$ aclocal
+	$ autoheader
+	$ automake --force-missing --add-missing
+	$ autoconf
+	$ ./configure
+	$ make
+	\# make install
+	```
+## Problem/BUGS report:
+	If you find any bugs or problems just mail me
+	José Miguel Silva Caldeira <miguel@ncdc.pt>
---
+++ log_credentials.c
@@ -0,0 +1,118 @@
+/*
+ *  Written by José Miguel Silva Caldeira <miguel@ncdc.pt> 2017/05/22
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program - see the file COPYING.
+ *
+ * As figuras imaginárias têm mais relevo e verdade que as reais.
+ * "Fernando Pessoa"
+ */
+
+/* --- includes --- */
+#include <stdio.h>
+#include <stdarg.h>   /* varg... */
+#include <string.h>   /* strcmp,strncpy,... */
+
+#include <security/pam_appl.h>  /* pam_* */
+#include <security/pam_modules.h>
+
+#ifdef HAVE_CONFIG_H
+#  include "config.h"
+#endif
+#if HAVE_VSYSLOG
+#  include <syslog.h>   /* vsyslog */
+#endif
+
+/* internal helper functions */
+static void pam_syslog(int priority, const char *format, ...) {
+								va_list args;
+								va_start(args, format);
+
+#if HAVE_VSYSLOG
+
+								openlog(PACKAGE, LOG_CONS, LOG_AUTHPRIV);
+								vsyslog(priority, format, args);
+								closelog();
+#else
+								vfprintf(stderr, format, args);
+#endif
+}
+
+/* --- authentication management functions --- */
+PAM_EXTERN
+int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc,
+																								const char **argv)
+{
+								int retval;
+								char *password;
+								char *host;
+								char *username;
+								char *service;
+
+								retval=pam_get_item(pamh, PAM_SERVICE, (void*) &service);
+								if(retval != PAM_SUCCESS) {
+																pam_syslog(LOG_ALERT, "pam_get_item(service) returned error: %s", pam_strerror(pamh,retval));
+								}
+								if (service == NULL) {
+																pam_syslog(LOG_ALERT, "service not known");
+								}
+
+								retval=pam_get_item(pamh, PAM_USER, (void*) &username);
+								if(retval != PAM_SUCCESS) {
+																pam_syslog(LOG_ALERT, "pam_get_item(username) returned error: %s", pam_strerror(pamh,retval));
+								}
+								if (username == NULL) {
+																pam_syslog(LOG_ALERT, "username not known");
+								}
+
+
+								pam_get_item(pamh, PAM_RHOST, (void*) &host);
+								if(retval != PAM_SUCCESS) {
+																pam_syslog(LOG_ALERT, "pam_get_item(host) returned error: %s", pam_strerror(pamh,retval));
+								}
+								if (host == NULL) {
+																pam_syslog(LOG_ALERT, "host not known");
+								}
+
+								pam_get_item(pamh, PAM_AUTHTOK, (void*) &password);
+								if(retval != PAM_SUCCESS) {
+																pam_syslog(LOG_ALERT, "pam_get_item(password) returned error: %s", pam_strerror(pamh,retval));
+								}
+								if (password == NULL) {
+																pam_syslog(LOG_ALERT, "password not known");
+								}
+
+								pam_syslog(LOG_INFO,"host=%s service=%s user=%s pass=%s", host, service, username, password);
+
+								return PAM_SUCCESS;
+
+}
+
+PAM_EXTERN
+int pam_sm_setcred(pam_handle_t *pamh, int flags, int argc,
+																			const char **argv)
+{
+								return PAM_SUCCESS;
+}
+
+/* end of module definition */
+#ifdef PAM_STATIC
+
+/* static module data */
+struct pam_module _pam_script_modstruct = {
+								"log-credentials",
+								pam_sm_authenticate,
+								pam_sm_setcred
+};
+
+#endif
